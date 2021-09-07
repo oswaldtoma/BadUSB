2e7301fa1ea8abcfaa9a4965893d4c2977453e91
Oswald Toma <oswald.toma@gmail.com>
2021-09-07 20:43:40 +0200

    Update

--- BadUSB.ino
+++ BadUSB.ino
@@ -1,13 +1,17 @@
 #include "hidkeyboard.h"
+#include "SD.h"
+#include "SPI.h"
 
-HIDkeyboard keyboard;
+HIDkeyboard key;
+SPIClass spi;
 
 void setup() {
   // put your setup code here, to run once:
-
+  key.begin();
+  spi.begin(15,16,17,-1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(1000);
 }
