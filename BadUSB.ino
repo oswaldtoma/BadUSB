d09cda9e26bd1b6bce33a3b458d1e6affa7b2148
Oswald Toma <oswald.toma@gmail.com>
2021-09-13 21:18:58 +0200

    HID and MSC tests

--- BadUSB.ino
+++ BadUSB.ino
@@ -1,17 +1,53 @@
 #include "hidkeyboard.h"
-#include "SD.h"
-#include "SPI.h"
+#include "sdusb.h"
+#include "Defines.h"
 
-HIDkeyboard key;
-SPIClass spi;
+#if CFG_TUD_HID
+// SDCard2USB sdUsb; //obiekt koliduje z hidem
+HIDkeyboard keyboard;
 
-void setup() {
-  // put your setup code here, to run once:
-  key.begin();
-  spi.begin(15,16,17,-1);
+void log(const char* str)
+{
+    Serial.println(str);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1000);
+bool isBootButtonClicked()
+{
+    return digitalRead(BOOT_BUTTON) == LOW;
 }
+
+void setup()
+{
+    pinMode(BOOT_BUTTON, INPUT_PULLUP);
+
+    Serial.begin(115200);
+
+    // if(!sdUsb.initSD(SD_SCK, SD_MISO, SD_MOSI, SD_CS))
+    // {
+    //     log("Failed to init SD");
+    // }
+
+    keyboard.begin();
+    
+    // if(!sdUsb.begin())
+    // {
+    //     log("LUN 1 failed");
+    // }
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    delay(200);
+
+    if(isBootButtonClicked())
+    {
+        keyboard.sendChar('a');
+        log("klik!");
+        // if(sdUsb.begin())
+        // {
+        //     log("sd started");
+        // }
+    }
+}
+#endif
---
+++ Defines.h
@@ -0,0 +1,6 @@
+#define SD_MISO  37
+#define SD_MOSI  35
+#define SD_SCK   36
+#define SD_CS    34
+
+#define BOOT_BUTTON 0
