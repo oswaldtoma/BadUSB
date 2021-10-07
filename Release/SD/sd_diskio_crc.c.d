19f26f428232145a5fb06351ef135742e273989f
Oswald Toma <oswald.toma@gmail.com>
2021-10-07 18:24:50 +0200

    adapt project to visual micro

---
+++ Release/SD/sd_diskio_crc.c.d
@@ -0,0 +1,2 @@
+C:\Users\Oswald\AppData\Local\Temp\VMBuilds\BadUSB\esp32_esp32s2\Release\SD\sd_diskio_crc.c.o: \
+ C:\Users\Oswald\AppData\Local\arduino15\packages\esp32\hardware\esp32\2.0.0\libraries\SD\src\sd_diskio_crc.c
