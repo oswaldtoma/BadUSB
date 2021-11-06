5dc3315397b2a9978330c5c00211f347e43a1182
Oswald Toma <oswald.toma@gmail.com>
2021-11-06 11:06:06 +0100

    Buttons are operational

--- BadUSB/WifiManager.cpp
+++ BadUSB/WifiManager.cpp
@@ -36,18 +36,37 @@ void WifiManager::run()
                                 <head> \
                                 </head> \
                                 <body> \
+                                    <script> \
+                                        function scrollToBottom() \
+                                        { \
+                                            var code = document.getElementById(\"code\"); \
+                                            code.scrollTop = code.scrollHeight; \
+                                        } \
+                                        \
+                                        function addString(value) { \
+                                            document.getElementById(\"code\").value += \"\\n\" + value + \" \"; \
+                                            scrollToBottom(); \
+                                        } \
+                                        \
+                                        function execute() { \
+                                        \
+                                        } \
+                                    </script> \
                                     <div id=\"main\" style=\"display: flex; height: 75%;\"> \
                                         <div id=\"text\" style=\"flex: 3;\"> \
-                                            <textarea name=\"code\" placeholder=\"Enter your code here.\" autofocus=\"true\" style=\"resize: none; width: 80%; height: 100%; font-size: 18px;\"></textarea> \
+                                            <textarea id=\"code\" placeholder=\"Enter your code here.\" autofocus=\"true\" style=\"resize: none; width: 80%; height: 100%; font-size: 18px;\"></textarea> \
                                         </div> \
                                         <div id = \"buttons\" style = \"flex: 1\"> \
-                                            <input type = \"button\" value = \"Comment\" style = \"padding: 50px 54px; font-size: 20px;\"><br><br><br><br> \
-                                            <input type = \"button\" value = \"Wait\" style = \"padding: 30px 34px; font-size: 20px;\"><br><br><br><br> \
-                                            <input type = \"button\" value = \"OS\" style = \"padding: 30px 34px; font-size: 20px;\"><br><br><br><br> \
-                                            <input type = \"button\" value = \"Context menu\" style = \"padding: 30px 34px; font-size: 20px;\"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> \
-                                            <input type = \"button\" value = \"Execute\" style = \"padding: 30px 34px; font-size: 20px;\"><br><br><br><br> \
+                                            <input type = \"button\" value = \"Comment\" style = \"padding: 50px 54px; font-size: 20px;\" onclick=\"addString(\'COM\')\"><br><br><br><br> \
+                                            <input type = \"button\" value = \"Wait\" style = \"padding: 30px 34px; font-size: 20px;\" onclick=\"addString(\'WT\')\"><br><br><br><br> \
+                                            <input type = \"button\" value = \"OS\" style = \"padding: 30px 34px; font-size: 20px;\" onclick=\"addString(\'OS\')\"><br><br><br><br> \
+                                            <input type = \"button\" value = \"Context menu\" style = \"padding: 30px 34px; font-size: 20px;\" onclick=\"addString(\'CON\')\"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> \
+                                            <input type = \"button\" value = \"Execute\" style = \"padding: 30px 34px; font-size: 20px;\" onclick=\"execute()\"><br><br><br><br> \
                                         </div> \
                                     </div> \
+                                    <script> \
+                                        scrollToBottom(); \
+                                    </script> \
                                 </body> \
                             </html>";
 
